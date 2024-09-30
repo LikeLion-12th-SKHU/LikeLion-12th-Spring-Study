a22110209f8c4682f6fbb0407552752a5727bf26
Lee, yeeun <ye7426@naver.com>
2024-10-01 06:54:18 +0900

    4주차 과제

---
+++ jpashop/src/main/java/org/skhu/jpashop/service/OrderService.java
@@ -0,0 +1,64 @@
+package org.skhu.jpashop.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.skhu.jpashop.domain.*;
+import org.skhu.jpashop.domain.item.Item;
+import org.skhu.jpashop.repository.ItemRepository;
+import org.skhu.jpashop.repository.MemberRepository;
+import org.skhu.jpashop.repository.OrderRepository;
+import org.skhu.jpashop.repository.OrderSearch;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+
+    //검색
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
+}
